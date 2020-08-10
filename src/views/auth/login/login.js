8a8a297cd8fd6a01bb3683a7cc40d4416b36c6ff
Gustavo Rosa <gth.rosa@uol.com.br>
2020-08-10 18:57:11 -0300

    Adding link to routes in auth components.

--- src/views/auth/login/login.js
+++ src/views/auth/login/login.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Login() {
-    return <div className="login">Login</div>;
+    return (
+        <div className="login">
+            <Link to={"/register"} className="btn btn-info">
+                Login
+            </Link>
+        </div>
+    );
 }
 
 export default Login;
--- src/views/auth/register/register.js
+++ src/views/auth/register/register.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Register() {
-    return <div className="register">Register</div>;
+    return (
+        <div className="register">
+            <Link to={"/login"} className="btn btn-danger">
+                Register
+            </Link>
+        </div>
+    );
 }
 
 export default Register;
