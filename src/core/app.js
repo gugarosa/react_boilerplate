06117dc4ea5f44fe73db1a0af337c69e6fa742a7
Gustavo Rosa <gth.rosa@uol.com.br>
2020-08-11 17:23:26 -0300

    Adding FontAwesome to project.

--- src/core/app.js
+++ src/core/app.js
@@ -1,5 +1,7 @@
 import React from "react";
+
 import "./app.scss";
+import "./icons";
 
 import Routes from "./routes";
 
--- src/views/auth/login/login.js
+++ src/views/auth/login/login.js
@@ -1,14 +1,37 @@
 import React from "react";
 import "./login.scss";
 
-import { Col, Container, Form, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Login() {
     return (
         <Container>
             <Row className="login">
-                <Col className="login-form">
-                    <Form className="login-form__wrapper">Lala</Form>
+                <Col xs="12" sm="8" md="6" lg="5" xl="4" className="login-form">
+                    <Form className="login-form__wrapper">
+                        <Form.Group controlId="email">
+                            <Form.Label>E-mail</Form.Label>
+                            <Form.Control type="email" placeholder="Digite o e-mail" />
+                        </Form.Group>
+                        <Form.Group controlId="password">
+                            <Form.Label>Senha</Form.Label>
+                            <Form.Control type="password" placeholder="Senha" />
+                        </Form.Group>
+                        <Form.Row className="justify-content-between">
+                            <Form.Group controlId="rememberMe">
+                                <Form.Check label="Lembrar-me" />
+                            </Form.Group>
+                            <Form.Group controlId="rememberMe">
+                                <Form.Label>Esqueceu a senha?</Form.Label>
+                            </Form.Group>
+                        </Form.Row>
+                        <Form.Row className="justify-content-center">
+                            <Button variant="primary">
+                                <FontAwesomeIcon icon={["fal", "sign-in-alt"]} /> Login
+                            </Button>
+                        </Form.Row>
+                    </Form>
                 </Col>
             </Row>
         </Container>
