a870630cd4ceb148e3e865a8a3b1a61f8c9655d2
Gustavo Rosa <gth.rosa@uol.com.br>
2020-08-11 20:48:40 -0300

    Improving login layout.

--- src/views/auth/login/login.js
+++ src/views/auth/login/login.js
@@ -1,48 +1,57 @@
 import React from "react";
 import "./login.scss";
 
-import { Button, Col, Container, Form, Image, Row } from "react-bootstrap";
+import { Button, Col, Container, Form, Image, InputGroup, Row } from "react-bootstrap";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 import logo from "../../../assets/img/logo.svg";
 
 function Login() {
     return (
-        <Container>
-            <Row className="login">
-                <Col xs="12" sm="8" md="6" lg="5" xl="4" className="login-form">
-                    <Form className="login-form__wrapper">
-                        <Form.Row className="justify-content-center">
-                            <Image className="login-form__wrapper-img" src={logo} />
-                        </Form.Row>
-                        <Form.Row className="justify-content-center">
-                            <h3>Vivere</h3>
-                        </Form.Row>
-                        <Form.Group controlId="email">
-                            <Form.Label>E-mail</Form.Label>
-                            <Form.Control type="email" placeholder="Digite o e-mail" />
-                        </Form.Group>
-                        <Form.Group controlId="password">
-                            <Form.Label>Senha</Form.Label>
-                            <Form.Control type="password" placeholder="Senha" />
-                        </Form.Group>
-                        <Form.Row className="justify-content-between">
-                            <Form.Group controlId="rememberMe">
-                                <Form.Check label="Lembrar-me" />
-                            </Form.Group>
-                            <Form.Group controlId="forgotPassword">
-                                <Form.Label>Esqueceu a senha?</Form.Label>
-                            </Form.Group>
-                        </Form.Row>
-                        <Form.Row className="justify-content-center">
-                            <Button variant="primary">
-                                <FontAwesomeIcon icon={["far", "sign-in"]} /> Login
-                            </Button>
-                        </Form.Row>
-                    </Form>
-                </Col>
-            </Row>
-        </Container>
+        <div id="login">
+            <Container>
+                <Row className="login">
+                    <Col xs="12" sm="10" md="10" lg="7" className="login-box">
+                        <Row className="justify-content-around">
+                            <Image src={logo} />
+                            <Form className="login-box__form">
+                                <Form.Group className="text-center mb-4">
+                                    <h3>Vivere</h3>
+                                </Form.Group>
+                                <Form.Group controlId="email">
+                                    <InputGroup>
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text>
+                                                <FontAwesomeIcon icon={["far", "envelope"]} />
+                                            </InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <Form.Control type="email" placeholder="E-mail" />
+                                    </InputGroup>
+                                </Form.Group>
+                                <Form.Group className="mb-4" controlId="password">
+                                    <InputGroup>
+                                        <InputGroup.Prepend>
+                                            <InputGroup.Text>
+                                                <FontAwesomeIcon icon={["far", "lock"]} />
+                                            </InputGroup.Text>
+                                        </InputGroup.Prepend>
+                                        <Form.Control type="password" placeholder="Senha" />
+                                    </InputGroup>
+                                </Form.Group>
+                                <Form.Group className="mb-3">
+                                    <Button variant="primary" block>
+                                        <FontAwesomeIcon icon={["far", "sign-in"]} /> Login
+                                    </Button>
+                                </Form.Group>
+                                <Form.Group className="text-center mb-0">
+                                    <Form.Label>Esqueceu a senha?</Form.Label>
+                                </Form.Group>
+                            </Form>
+                        </Row>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 }
 
