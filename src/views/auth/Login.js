cfde03d102f584697ef19792479116a762dd0a7c
Gustavo Rosa <gth.rosa@uol.com.br>
2020-08-18 18:39:40 -0300

    Improving login page and stopping use react-bootstrap.

--- src/views/auth/Login.js
+++ src/views/auth/Login.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
+import { useForm } from "react-hook-form";
+import { Link } from "react-router-dom";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Button, Col, Container, Form, Image, InputGroup, Row } from "react-bootstrap";
 
 import { AuthContext } from "../../context/Auth";
 
@@ -8,67 +9,68 @@ import "./auth.scss";
 import logo from "../../assets/img/logo.svg";
 
 export const Login = () => {
-    const { login, logout } = useContext(AuthContext);
+    const { login } = useContext(AuthContext);
+    const { register, handleSubmit } = useForm();
+
+    const onSubmit = (data) => {
+        console.log(data);
+    };
 
     return (
-        <div id="login">
-            <Container>
-                <Row className="login">
-                    <Col xs="12" sm="12" md="10" lg="7" className="login-box">
-                        <Row className="justify-content-around">
-                            <Image src={logo} />
-                            <div className="login-box__divider d-none d-sm-block"></div>
-                            <Form className="login-box__form">
-                                <Form.Group className="text-center mb-4">
-                                    <h3>Humana Alimentar</h3>
-                                    <span className="text-muted">Painel de Admnistração</span>
-                                </Form.Group>
-                                <Form.Group controlId="email">
-                                    <InputGroup>
-                                        <InputGroup.Prepend>
-                                            <InputGroup.Text>
-                                                <FontAwesomeIcon icon={["far", "envelope"]} />
-                                            </InputGroup.Text>
-                                        </InputGroup.Prepend>
-                                        <Form.Control type="email" placeholder="E-mail" />
-                                    </InputGroup>
-                                </Form.Group>
-                                <Form.Group className="mb-4" controlId="password">
-                                    <InputGroup>
-                                        <InputGroup.Prepend>
-                                            <InputGroup.Text>
-                                                <FontAwesomeIcon icon={["far", "lock"]} />
-                                            </InputGroup.Text>
-                                        </InputGroup.Prepend>
-                                        <Form.Control type="password" placeholder="Senha" />
-                                    </InputGroup>
-                                </Form.Group>
-                                <Form.Group className="mb-2">
-                                    <Button
-                                        className="text-uppercase font-weight-bold"
-                                        variant="primary"
-                                        block
-                                        onClick={() => login({ user: "user", token: "token" })}
-                                    >
-                                        Login
-                                    </Button>
-                                    <Button
-                                        className="text-uppercase font-weight-bold"
-                                        variant="danger"
-                                        block
-                                        onClick={() => logout()}
-                                    >
-                                        Logout
-                                    </Button>
-                                </Form.Group>
-                                <Form.Group className="text-center mb-0">
-                                    <Button variant="link">Esqueceu a senha?</Button>
-                                </Form.Group>
-                            </Form>
-                        </Row>
-                    </Col>
-                </Row>
-            </Container>
+        <div className="container-fluid">
+            <div className="row login">
+                <div className="col-md-6 col-xl-5 p-4 p-md-5 login-main">
+                    <div className="row no-gutters align-items-center mb-4 login-main__header">
+                        <img className="mr-1" src={logo} alt="Humana Life" />
+                        <span className="font-weight-bold">Humana Life</span>
+                    </div>
+                    <div className="mb-5">
+                        <h2 className="font-weight-bold">Bem-vindo!</h2>
+                        <h4>Painel de Admnistração</h4>
+                    </div>
+                    <form className="mb-4" onSubmit={handleSubmit(onSubmit)}>
+                        <div className="form-group">
+                            <label className="font-weight-l-bold">E-mail</label>
+                            <input
+                                className="form-control"
+                                name="email"
+                                type="email"
+                                placeholder="nome@email.com"
+                                ref={register}
+                            />
+                        </div>
+                        <div className="form-group mb-4">
+                            <label className="font-weight-l-bold">Senha</label>
+                            <input
+                                className="form-control"
+                                name="password"
+                                type="password"
+                                placeholder="********"
+                                ref={register}
+                            />
+                        </div>
+                        <button
+                            className="btn btn-primary btn-block font-weight-bold"
+                            onClick={() => login({ user: "user", token: "token" })}
+                        >
+                            <FontAwesomeIcon icon={["far", "sign-in-alt"]} /> Entrar
+                        </button>
+                    </form>
+                    <hr></hr>
+                    <div className="mt-auto">
+                        <Link to={"/forgot-password"} className="btn btn-link p-0 mb-2">
+                            Esqueci a senha
+                        </Link>
+                        <div>
+                            <span className="mr-1 font-weight-l-bold">Não possui uma conta?</span>
+                            <Link to={"/register"} className="btn btn-link align-baseline p-0">
+                                Registrar
+                            </Link>
+                        </div>
+                    </div>
+                </div>
+                <div className="col-md-6 col-xl-7 d-none d-md-block login-alt"></div>
+            </div>
         </div>
     );
 };
