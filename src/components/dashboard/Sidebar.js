7563f0495b4cdab0f66d2a3a85a8c31d7ead94cc
Gustavo Rosa <gth.rosa@uol.com.br>
2020-08-19 11:47:05 -0300

    Working on sidebar.

--- src/components/dashboard/Sidebar.js
+++ src/components/dashboard/Sidebar.js
@@ -1,18 +1,26 @@
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import "./dashboard.scss";
 
 export const Sidebar = () => {
     return (
         <nav className="sidebar">
-            <ul className="sidebar-nav">
-                <li className="sidebar-nav__item">
-                    <a href="/#">
-                        <span>Cats</span>
+            <div className="sidebar-header">
+                <span className="sidebar-header__text">Olaf</span>
+                <FontAwesomeIcon icon={["far", "sign-in-alt"]} />
+            </div>
+            <ul className="sidebar-menu">
+                <li className="sidebar-menu__item">
+                    <a className="sidebar-menu__item-link" href="/#">
+                        <FontAwesomeIcon icon={["far", "sign-in-alt"]} />
+                        <span className="sidebar-menu__item-link-text">Cats</span>
                     </a>
                 </li>
-                <li className="sidebar-nav__item">
-                    <a href="/#">
-                        <span>Dogs</span>
+                <li className="sidebar-menu__item">
+                    <a className="sidebar-menu__item-link" href="/#">
+                        <FontAwesomeIcon icon={["far", "sign-out-alt"]} />
+                        <span className="sidebar-menu__item-link-text">Dogs</span>
                     </a>
                 </li>
             </ul>
